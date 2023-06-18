79b363b46c9334c353f2678d481d1895115fcde5
Guilherme Lima Desoler <guilherme.desoler@gmail.com>
2023-06-17 23:42:58 -0300

    refactor: change connectivity to plus

--- lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
+++ lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_project/core/error/exceptions.dart';
 
-import 'package:flutter_project/core/platform/network_info.dart';
+import 'package:flutter_project/core/network/network_info.dart';
 import 'package:flutter_project/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
 import 'package:flutter_project/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
 
